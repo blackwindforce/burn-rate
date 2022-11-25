d2436fbf7f9eb8f9607a6a46082879d0f08b36d6
Akira Cai <blackwindforce@users.noreply.github.com>
2022-11-25 19:36:32 +0800

    Add files via upload

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  root: true,
+};
